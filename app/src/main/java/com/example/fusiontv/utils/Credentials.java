ba01403dadddd7518a666e3f5d4b0baeed4caf5c
Dana Shorts <32418263+csudhtoro@users.noreply.github.com>
2024-02-06 15:10:34 -0800

    Update Credentials.java

--- app/src/main/java/com/example/fusiontv/utils/Credentials.java
+++ app/src/main/java/com/example/fusiontv/utils/Credentials.java
@@ -3,6 +3,6 @@ package com.example.fusiontv.utils;
 public class Credentials {
 
     public static final String Base_URL = "https://api.themoviedb.org";
-    public static final String API_KEY = "5711ccb17a8987bca87b6e6fd7dc4823";
+    public static final String API_KEY = "XXXXXX";
 
 }
